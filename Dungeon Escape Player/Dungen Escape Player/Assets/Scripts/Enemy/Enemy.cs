7510e05e0c19578faa5fdc7f31160f3048d2b57a
moe4546 <horn.moritz@web.de>
2019-03-05 18:38:11 +0100

    a

---
+++ Dungeon Escape Player/Dungen Escape Player/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    protected int health;
+    protected int speed;
+    protected int gems;
+
+    public void Attack()
+    {
+
+    }
+
+}
---
+++ Dungeon Escape Player/Dungen Escape Player/Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _animator;
+    private Animator _swordAnimator;
+    private SpriteRenderer _spriteRenderer;
+    private SpriteRenderer _swordRenderer;
+    private Transform _swordTransform;
+
+
+    private void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        _swordAnimator = transform.GetChild(1).GetComponent<Animator>();
+        _swordRenderer = transform.GetChild(1).GetComponent<SpriteRenderer>();
+        _swordTransform = transform.GetChild(1).transform;
+    }
+
+    public void Run(float running)
+    {
+        _animator.SetFloat("running", Mathf.Abs(running));
+
+        if(running < 0) 
+        {
+            _spriteRenderer.flipX = true;
+            _swordRenderer.flipX = true;
+            _swordRenderer.flipY = true;
+            Vector3 newPos = _swordTransform.localPosition;
+            newPos.x = -1.01f;
+            _swordTransform.localPosition = newPos;
+        }
+        else if(running > 0) 
+        {
+            _spriteRenderer.flipX = false;
+            _swordRenderer.flipX = false;
+            _swordRenderer.flipY = false;
+            Vector3 newPos = _swordTransform.localPosition;
+            newPos.x = 1.01f;
+            Debug.Log(newPos);
+            _swordTransform.localPosition = newPos;
+        }
+    }
+
+    public void Jump(bool jumping)
+    {
+        _animator.SetBool("jumping", jumping);
+    }
+
+    public void Attack()
+    {
+        _animator.SetTrigger("Attack");
+        _swordAnimator.SetTrigger("SwordAnimation");
+    }
+}
