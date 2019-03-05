5a88ca01f146d78e7e363bba5566071ddd2dbf6e
moe4546 <horn.moritz@web.de>
2019-03-05 07:26:21 +0100

    Sword Animation

--- Dungeon Escape Player/Dungen Escape Player/Assets/Scripts/PlayerAnimation.cs
+++ Dungeon Escape Player/Dungen Escape Player/Assets/Scripts/PlayerAnimation.cs
@@ -6,14 +6,18 @@ public class PlayerAnimation : MonoBehaviour
 {
     private Animator _animator;
     private Animator _swordAnimator;
-    private SpriteRenderer _sprteRenderer;
+    private SpriteRenderer _spriteRenderer;
+    private SpriteRenderer _swordRenderer;
+    private Transform _swordTransform;
 
 
     private void Start()
     {
         _animator = GetComponentInChildren<Animator>();
-        _sprteRenderer = GetComponentInChildren<SpriteRenderer>();
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         _swordAnimator = transform.GetChild(1).GetComponent<Animator>();
+        _swordRenderer = transform.GetChild(1).GetComponent<SpriteRenderer>();
+        _swordTransform = transform.GetChild(1).transform;
     }
 
     public void Run(float running)
@@ -22,11 +26,22 @@ public class PlayerAnimation : MonoBehaviour
 
         if(running < 0) 
         {
-            _sprteRenderer.flipX = true;
+            _spriteRenderer.flipX = true;
+            _swordRenderer.flipX = true;
+            _swordRenderer.flipY = true;
+            Vector3 newPos = _swordTransform.localPosition;
+            newPos.x = -1.01f;
+            _swordTransform.localPosition = newPos;
         }
         else if(running > 0) 
         {
-            _sprteRenderer.flipX = false;
+            _spriteRenderer.flipX = false;
+            _swordRenderer.flipX = false;
+            _swordRenderer.flipY = false;
+            Vector3 newPos = _swordTransform.localPosition;
+            newPos.x = 1.01f;
+            Debug.Log(newPos);
+            _swordTransform.localPosition = newPos;
         }
     }
 
