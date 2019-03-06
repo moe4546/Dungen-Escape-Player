eb4db44c3020ecd2dd7f2f56cda8d9bda78c6594
moe4546 <horn.moritz@web.de>
2019-03-06 07:38:17 +0100

    Moss Giant

--- Dungeon Escape Player/Dungen Escape Player/Assets/Scripts/Enemy/Enemy.cs
+++ Dungeon Escape Player/Dungen Escape Player/Assets/Scripts/Enemy/Enemy.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
-    protected int health;
-    protected int speed;
-    protected int gems;
+    [SerializeField] protected int health;
+    [SerializeField] protected float speed;
+    [SerializeField] protected int gems;
+    [SerializeField] protected Transform pointA, pointB;
+    protected Transform lastPos;
+    protected Animator animator;
+    protected string currentAnimation;
+    protected SpriteRenderer spriteRenderer;
 
-    public void Attack()
+    public virtual void Start()
+    {
+        animator = transform.GetChild(0).GetComponent<Animator>();
+        spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        transform.position = pointA.position;
+        lastPos = pointA;
+    }
+
+    public virtual void Attack()
     {
 
     }
+
+    public abstract void Update();
 
 }
--- Dungeon Escape Player/Dungen Escape Player/Assets/Scripts/Enemy/MossGiant.cs
+++ Dungeon Escape Player/Dungen Escape Player/Assets/Scripts/Enemy/MossGiant.cs
@@ -4,5 +4,51 @@ using UnityEngine;
 
 public class MossGiant : Enemy
 {
-    
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Update()
+    {
+        SetCurrentAnimation();
+
+        if (currentAnimation != "Idle")
+        {
+            Patrol();
+        }
+    }
+
+    void Patrol()
+    {
+        // Move
+        if (lastPos == pointA)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, pointB.position, speed * Time.deltaTime);
+        }
+        else if (lastPos == pointB)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, pointA.position, speed * Time.deltaTime);
+        }
+
+        // Reach Point?
+        if(transform.position.x >= pointB.position.x)
+        {
+            lastPos = pointB;
+            animator.Play("Idle");
+            spriteRenderer.flipX = true;
+        }
+        else if(transform.position.x <= pointA.position.x)
+        {
+            lastPos = pointA;
+            animator.Play("Idle");
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    private void SetCurrentAnimation()
+    {
+        AnimatorClipInfo[] clipInfos = animator.GetCurrentAnimatorClipInfo(0);
+        currentAnimation = clipInfos[0].clip.name;
+    }
 }
